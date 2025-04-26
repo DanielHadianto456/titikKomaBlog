a673d90e81102d7600aff2837860bb4b9bc53980
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 16:15:26 +0700

    style(footer): update heading styles for improved visibility

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer-section absolute bottom-0 w-[100%]">
-    <h3 class="font-montserrat text-center text-base text-[#FFFFFF] bg-[#02A28F] p-3">
+    <h3 class="font-montserrat text-center text-base font-semibold text-[#FFFFFF] bg-[#02A28F] p-4">
         Copyright@KangTutor
     </h3>
 </div>
