eab81bbdb0f41748eaf4928a18517ea7eb1180c7
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 14:35:31 +0700

    feat: add main article content with image and credentials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,20 @@
 <x-default-layout>
     <section class="header-section grid grid-cols-12 gap-6">
         <div class="main-article col-span-8">
-            <h2>test</h2>
+            <div class="image-container relative h-[65vh] w-[100%] rounded-xl drop-shadow-xl bg-cover bg-center"
+            style="background-image: url('https://cdn.britannica.com/22/187222-050-07B17FB6/apples-on-a-tree-branch.jpg')">
+                <div class="article-credentials absolute bottom-0 left-0 p-6 font-montserrat">
+                    <h3 class="article-category text-base">
+                        Fruits Category
+                    </h3>
+                    <h2 class="article-title text-4xl font-bold ">
+                        These are apples
+                    </h2>
+                    <h3 class="article-author text-base ">
+                        <strong>By</strong> John Doe
+                    </h3>
+                </div>
+            </div>
         </div>
         <div class="side-article col-span-4">
             <h2>test</h2>
