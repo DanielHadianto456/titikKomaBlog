e8847c5a9de0fde6a7af93500787f600ac40b3a1
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 12:22:04 +0700

    feat: enhance navbar style and add font awesome cdn

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,14 @@
-<div class="navbar flex justify-between mx-8">
+<div class="navbar bg-[#02A28F] flex justify-between items-center px-40 py-4">
     <div class="navbar-title">
-        <h1 class="text-4xl font-montserrat font-bold">Kang Tutor</h1>
+        <h1 class="text-4xl font-montserrat font-bold text-[#FFFFFF]">Kang Tutor</h1>
     </div>
-    <div class="navbar-items">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+    <div class="navbar-items font-montserrat font-medium flex gap-5">
+        <a href="#" class="text-[#FFFFFF]">Beranda</a>
+        <a href="#" class="text-[#FFFFFF]">Tutorial</a>
+        <a href="#" class="text-[#FFFFFF]">Pemrograman</a>
+        <a href="#" class="text-[#FFFFFF]">Teknologi</a>
+        <a href="#" class="text-[#FFFFFF]">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </a>
     </div>
 </div>
