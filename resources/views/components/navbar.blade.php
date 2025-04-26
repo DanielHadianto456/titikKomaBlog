c1fe95342c345e46a3b52c0c150e1f2524b1e5ec
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 11:11:07 +0700

    feat: add navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+    <div class="navbar-title">
+        <h1>Kang Tutor</h1>
+    </div>
+    <div class="navbar-items">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </div>
+</div>
