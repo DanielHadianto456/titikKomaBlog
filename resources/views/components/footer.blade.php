aa995ac341d7ef15e8c48f8122f2c84c0a3f8e9f
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 16:34:58 +0700

    style(footer): remove absolute positioning from footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="footer-section absolute bottom-0 w-[100%]">
+<div class="footer-section w-[100%]">
     <h3 class="font-montserrat text-center text-base font-semibold text-[#FFFFFF] bg-[#02A28F] p-4">
         Copyright@KangTutor
     </h3>
