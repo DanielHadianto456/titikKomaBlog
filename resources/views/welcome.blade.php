716e7507c5691894347051c7c84b969d93d66aca
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-04-26 12:48:11 +0700

    style(welcome): configure header section gridding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-default-layout>
-    
+    <section class="header-section grid grid-cols-12 gap-6">
+        <div class="main-article col-span-8">
+            <h2>test</h2>
+        </div>
+        <div class="side-article col-span-4">
+            <h2>test</h2>
+        </div>
+    </section>
 </x-default-layout>
