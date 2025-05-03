f50879ea1b3332928950d4801c7f7d7d32a344d8
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-05-03 11:22:18 +0700

    feat: create article and article category models

---
+++ app/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'author',
+        'date',
+        'content',
+        'image',
+        'article_category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ArticleCategory::class, 'article_category_id');
+    }
+}
