00e30f6081f57e548ad639eca9d5064b69183295
Daniel Hadianto <daniel88hadianto@gmail.com>
2025-05-17 11:17:57 +0700

    feat(admin article): add create article view

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -7,7 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4">
+                <div class="button-container mb-4">
+                    <a href="{{ route('admin.articles.create') }}"
+                        class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition ease-in-out duration-150">
+                        {{ __('Create Article') }}
+                    </a>
+                </div>
                 <div class="table-container">
                     {{-- <table class="min-w-full table-fixed ">
                         <thead>
